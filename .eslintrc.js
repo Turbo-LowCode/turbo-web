2597f3e9b9b9079711f849bd2696424449517a1c
JinSooo <kimjinso@qq.com>
2023-11-25 23:11:44 +0800

    feat: ✨ eslint for monorepo

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,4 @@
 module.exports = {
-	env: {
-		browser: true,
-		es2021: true,
-	},
-	extends: ['standard', 'standard-jsx'],
-	parser: '@typescript-eslint/parser',
-	plugins: ['prettier'],
-	rules: {
-		'prettier/prettier': 'error',
-		'no-unused-vars': 'off',
-		'no-useless-constructor': 'off',
-	},
+  root: true,
+  extends: ['@turbo/lint/index.js'],
 }
---
+++ packages/eslint/index.js
@@ -0,0 +1,17 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: ['standard', 'standard-jsx', 'plugin:@typescript-eslint/recommended', 'prettier'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint'],
+  rules: {
+    'prettier/prettier': 'error',
+  },
+}
