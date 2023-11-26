f0af099a73163ea81e31f5d092b4b9eedc27a762
JinSooo <kimjinso@qq.com>
2023-11-26 12:15:30 +0800

    feat: ✨ React物料组件的封装

---
+++ packages/core/src/utils/const.ts
@@ -0,0 +1,8 @@
+/** 画布的Id */
+export const CanvasRootId = '__TurboEditorPreview__'
+
+/** 实例的Id */
+export const ScopeModuleId = '__TurboBuilderScope__'
+
+/** 代码运行时的ID */
+export const RuntimeCtxId = '__TurboRuntime__'
