dd56f48617ab99a78153f2bf9e53f6f57dc17d8f
JinSo <kimjinso@qq.com>
2023-11-27 12:33:08 +0800

    fix(eslint-config): 🐛 更新eslint配置

--- packages/eslint-config/index.js
+++ packages/eslint-config/index.js
@@ -4,7 +4,7 @@ module.exports = {
     es2021: true,
     node: true,
   },
-  extends: ['standard', 'standard-jsx', 'plugin:@typescript-eslint/recommended', 'plugin:prettier/recommended'],
+  extends: ['standard', 'standard-jsx', 'plugin:@typescript-eslint/recommended', 'prettier'],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 'latest',
@@ -13,5 +13,13 @@ module.exports = {
   plugins: ['@typescript-eslint', 'prettier'],
   rules: {
     'prettier/prettier': 'error',
+    'arrow-body-style': 'off',
+    'prefer-arrow-callback': 'off',
   },
+  ignorePatterns: ['.*.js', 'node_modules/', 'dist/'],
+  overrides: [
+    {
+      files: ['*.js?(x)', '*.ts?(x)'],
+    },
+  ],
 }
