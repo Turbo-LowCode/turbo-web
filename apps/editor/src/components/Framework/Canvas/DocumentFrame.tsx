d77e4e085cf46fda90a82d0e800b5813f677c0d6
JinSo <kimjinso@qq.com>
2023-11-29 19:25:46 +0800

    fix(editor): 🐛 修复Element无效bug

--- apps/editor/src/components/Framework/Canvas/DocumentFrame.tsx
+++ apps/editor/src/components/Framework/Canvas/DocumentFrame.tsx
@@ -1,14 +1,14 @@
-import { Canvas, Frame } from '@craftjs/core'
-import { __AntDButton__, __Provider__ } from '@turbo/materials'
+import { Canvas, Element, Frame } from '@craftjs/core'
+import { __AntDButton__, __Box__, __Provider__ } from '@turbo/materials'
 
 export const DocumentFrame = () => {
   return (
     <div id="__CanvasViewPort__" className="w-screen h-screen ps-3 pe-3">
       <Frame>
         <Canvas canvas is={__Provider__} backgroundColor="#ffffff" width="100%" height="100%">
-          {/* <Element canvas is={__Box__}> */}
-          <__AntDButton__ />
-          {/* </Element> */}
+          <Element canvas is={__Box__}>
+            <__AntDButton__ />
+          </Element>
         </Canvas>
       </Frame>
     </div>
