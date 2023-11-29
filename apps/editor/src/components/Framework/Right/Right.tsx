4eca3bb6daf50f0b7f857b3d1185648c127442ee
JinSooo <kimjinso@qq.com>
2023-11-29 22:30:35 +0800

    feat(editor): ✨ Right Settings布局

--- apps/editor/src/components/Framework/Right/Right.tsx
+++ apps/editor/src/components/Framework/Right/Right.tsx
@@ -1,3 +1,49 @@
+import { Tabs, TabsProps, theme } from 'antd'
+import { createStyles } from 'antd-style'
+import { Events } from './Events'
+import { Settings } from './Settings'
+
+const useStyles = createStyles(({ token, css }) => ({
+  Tab: css`
+    height: '100%';
+    .ant-tabs-content-top {
+      height: '100%';
+    }
+    .ant-tabs-tabpane {
+      padding: ${token.paddingXS};
+      overflow: 'auto';
+      height: 'calc(100% - 80px)';
+      box-sizing: 'border-box';
+    }
+  `,
+}))
+
+const items: TabsProps['items'] = [
+  {
+    key: 'prototype',
+    label: '属性',
+    children: <Settings />,
+  },
+  {
+    key: 'event',
+    label: '事件',
+    children: <Events />,
+  },
+]
+
 export const Right = () => {
-  return <div>Right</div>
+  const { styles } = useStyles()
+  const { token } = theme.useToken()
+
+  return (
+    <div className="h-[100%_-_50px]">
+      <Tabs
+        className={styles.Tab}
+        size="small"
+        items={items}
+        tabBarStyle={{ paddingInline: token.paddingXS }}
+        defaultActiveKey="prototype"
+      />
+    </div>
+  )
 }
---
+++ apps/editor/src/components/Framework/Right/Settings.tsx
@@ -0,0 +1,79 @@
+import { useEditor } from '@craftjs/core'
+import { Card, Form, Typography } from 'antd'
+import { createStyles } from 'antd-style'
+import { merge } from 'lodash'
+import { useEffect } from 'react'
+
+const useStyles = createStyles(({ css }) => ({
+  Setting: css`
+    height: '100%';
+    & .ant-pro-form-group-title {
+      font-weight: 600;
+      margin-block-end: 0;
+      cursor: 'pointer';
+    }
+  `,
+}))
+
+export const Settings = () => {
+  const { styles } = useStyles()
+  const [form] = Form.useForm()
+  const {
+    nodeId,
+    props,
+    settingRender,
+    actions: { setProp },
+  } = useEditor(state => {
+    const [currentNodeId] = state.events.selected
+
+    if (currentNodeId) {
+      const { data, related } = state.nodes[currentNodeId]
+
+      return {
+        nodeId: currentNodeId,
+        props: data.props,
+        settingRender: related.settingRender,
+      }
+    }
+  })
+
+  const handleFormChange = (values: any) => {
+    if (nodeId) {
+      setProp(nodeId, setterProps => {
+        return merge(setterProps, values)
+      })
+    }
+    return true
+  }
+
+  useEffect(() => {
+    if (nodeId) {
+      console.log('currentNodeId', nodeId, props)
+
+      // 切换组件清除setter配置，并设置新组件内容属性配置
+      form.resetFields()
+      form.setFieldsValue(props)
+    }
+  }, [nodeId])
+
+  return (
+    <div className={styles.Setting}>
+      <Form
+        layout="vertical"
+        form={form}
+        labelCol={{ span: 24 }}
+        wrapperCol={{ span: 24 }}
+        onValuesChange={handleFormChange}
+        onFinish={handleFormChange}
+      >
+        {settingRender ? (
+          <settingRender />
+        ) : (
+          <Card size="small">
+            <Typography.Text type="secondary">暂无选中组件，点击画布中的组件可以进行选择。</Typography.Text>
+          </Card>
+        )}
+      </Form>
+    </div>
+  )
+}
