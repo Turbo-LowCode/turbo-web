81aadc8abec5634ebbae8a1977ea8758db11e716
JinSo <kimjinso@qq.com>
2023-12-01 19:25:13 +0800

    feat(lint): ✨ 通过脚本实现git hook 自执行并格式化

--- packages/lint/prepare.js
+++ packages/lint/prepare.js
@@ -1,50 +1,52 @@
 const isCi = process.env.CI !== undefined
 const path = require('path')
 const husky = require('husky')
-const fs = require('fs')
 const { execSync } = require('child_process')
 
-function isDiskRoot(path) {
-  const pathObj = require('path').parse(path)
-  pathObj.root === path
+const isDiskRoot = path => {
+  const pathObj = parse(path)
+  return pathObj.root === path
 }
 
-function hasGitFolder(dirPath) {
-  const gitFolderPath = `${dirPath}/.git`
+const isGitFolder = dirPath => {
+  const gitFolderPath = `${dirPath}\\.git`
   try {
-    const stat = fs.statSync(gitFolderPath)
+    const stat = statSync(gitFolderPath)
     return stat.isDirectory() || stat.isFile()
   } catch (error) {
     return false
   }
 }
-// 递归向上，找有git的文件夹
-function findNearestGit(dirname) {
+
+const findNearestGit = dirname => {
   if (isDiskRoot(dirname)) {
     return null
   }
-  if (hasGitFolder(dirname)) {
+  if (isGitFolder(dirname)) {
     return dirname
   } else {
-    return findNearestGit(path.resolve(dirname, '..'))
+    return findNearestGit(resolve(dirname, '..'))
   }
 }
 
 if (!isCi) {
   const gitFolder = findNearestGit(__dirname)
   if (!gitFolder) {
-    throw new Error('postinstall husky but cannot find .git file, please git init first!')
+    throw new Error(
+      'postinstall husky but cannot find .git file, please git init first!',
+    )
   }
 
-  const customDirectory = path.join(__dirname, 'husky')
   console.log('🚀 ~ Scan to git directory:', gitFolder)
   // execSync(`cd ${gitFolder} && husky install ${customDirectory}`)
   execSync(`cd ${gitFolder} && husky install`)
 
   const preCommitFile = path.join(gitFolder, '.husky', 'pre-commit')
-  const lintStagedJs = path.join(__dirname, 'runLintStaged.mjs').replace(/\\/g, '/')
+  const lintStagedJs = path
+    .join(__dirname, 'runLintStaged.mjs')
+    .replace(/\\/g, '/')
   husky.set(
     preCommitFile,
-    `echo ''\necho "\\e[0;32m ******* pre-commit 钩子触发 **************\\e[0m"\necho ''\n\nnode "${lintStagedJs}"`,
+    `echo ''\necho -e "\\e[0;32m ******* pre-commit 钩子触发 **************\\e[0m"\necho ''\n\nnode "${lintStagedJs}"`,
   )
 }
--- packages/lint/runLintStaged.mjs
+++ packages/lint/runLintStaged.mjs
@@ -1,7 +1,43 @@
+import { statSync } from 'fs'
 import lintStaged from 'lint-staged'
+import { dirname, parse, resolve } from 'path'
+import { fileURLToPath } from 'url'
+
+const isDiskRoot = path => {
+  const pathObj = parse(path)
+  return pathObj.root === path
+}
+
+const isGitFolder = dirPath => {
+  const gitFolderPath = `${dirPath}\\.git`
+  try {
+    const stat = statSync(gitFolderPath)
+    return stat.isDirectory() || stat.isFile()
+  } catch (error) {
+    return false
+  }
+}
+
+const findNearestGit = dirname => {
+  if (isDiskRoot(dirname)) {
+    return null
+  }
+  if (isGitFolder(dirname)) {
+    return dirname
+  } else {
+    return findNearestGit(resolve(dirname, '..'))
+  }
+}
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+// 通过查找git仓库找到项目根目录的位置
+const gitFolder = findNearestGit(__dirname)
+
+if (!gitFolder) throw new Error('🚀 ~ biome failed! please fix and retry!')
 
 lintStaged({
-  config: { '*.{js,ts,jsx,tsx,json,css,md}': `biome format --write .` },
+  config: { '*.{js,ts,jsx,tsx,json,css,md}': `biome format --write ${gitFolder}\\.` },
 }).then(res => {
   if (res) {
     console.log('🚀 ~ set lintStaged successfully!')
