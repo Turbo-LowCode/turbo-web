641b62f8dc468b07fe787db24acab602608f394e
JinSooo <kimjinso@qq.com>
2023-12-10 13:49:59 +0800

    feat(editor): ✨ 提供修改物料属性的方法

---
+++ apps/editor/src/components/EditorFramework/Editor.tsx
@@ -0,0 +1,68 @@
+import { useEditor } from '@craftjs/core'
+import { connectJSRuntimeVM, mountJSModule } from '@turbolc/core'
+import dayjs from 'dayjs'
+import { FC, useEffect } from 'react'
+import { Canvas } from './Canvas/Canvas'
+import { DocumentFrame } from './Canvas/DocumentFrame'
+import { Header } from './Header/Header'
+import { Left } from './Left/Left'
+import { Right } from './Right/Right'
+import { useSchemaStore } from './stores/schema'
+
+export interface EditorProps {
+  isPreview: boolean
+}
+
+export const Editor: FC<EditorProps> = ({ isPreview }) => {
+  const jsModuleCode = useSchemaStore(state => state.jsModuleCode)
+  const { actions, nodeId } = useEditor(state => {
+    const [currentNodeId] = state.events.selected
+    return {
+      nodeId: currentNodeId,
+    }
+  })
+
+  useEffect(() => {
+    window.__nodeId__ = nodeId
+  }, [nodeId])
+
+  // 初始化js模块
+  useEffect(() => {
+    mountJSModule(jsModuleCode)
+  }, [jsModuleCode])
+
+  // 初始化js模块默认依赖
+  useEffect(() => {
+    const { sandbox } = connectJSRuntimeVM()
+    sandbox.turboScope.dependencies = {
+      dayjs,
+      '@turbolc/store': {
+        getState: () => {
+          console.log('我是get方法')
+        },
+        setState: () => {
+          console.log('我是set方法')
+        },
+      },
+      // 透传一些改变物料的方法
+      '@turbolc/materials': {
+        setProp: (cb: (props: any) => void) => {
+          actions.setProp(window.__nodeId__, cb)
+        },
+      },
+    }
+  }, [])
+
+  return isPreview ? (
+    <DocumentFrame />
+  ) : (
+    <div className='grid overflow-hidden h-full grid-rows-[auto_1fr]'>
+      <Header />
+      <div id='editorContent' className='h-full grid grid-cols-[auto_1fr_300px]'>
+        <Left />
+        <Canvas />
+        <Right />
+      </div>
+    </div>
+  )
+}
--- apps/editor/src/components/EditorFramework/EditorFramework.tsx
+++ apps/editor/src/components/EditorFramework/EditorFramework.tsx
@@ -1,52 +1,16 @@
-import {
-  EditorRefProps,
-  FrameworkContextProvider,
-  FrameworkProviderProps,
-  connectJSRuntimeVM,
-  mountJSModule,
-} from '@turbolc/core'
+import { EditorRefProps, FrameworkContextProvider, FrameworkProviderProps } from '@turbolc/core'
 import { __antdMaterials__, __baseMaterials__, __commonMaterials__, __formMaterials__ } from '@turbolc/materials'
-import dayjs from 'dayjs'
-import { forwardRef, useEffect } from 'react'
-import { Canvas } from './Canvas/Canvas'
-import { DocumentFrame } from './Canvas/DocumentFrame'
-import { Header } from './Header/Header'
-import { Left } from './Left/Left'
+import { forwardRef } from 'react'
+import { Editor } from './Editor'
 import { RenderNodeWrapper } from './RenderNodeWrapper'
-import { Right } from './Right/Right'
-import { useSchemaStore } from './stores/schema'
 
 export type FrameworkProps = FrameworkProviderProps & {
   schema?: string
   isPreview?: boolean
-  // children?: React.ReactNode
 }
 
 export const EditorFramework = forwardRef<EditorRefProps, FrameworkProps>(
   ({ enabled = true, isPreview = false }, ref) => {
-    const jsModuleCode = useSchemaStore(state => state.jsModuleCode)
-
-    // 初始化js模块
-    useEffect(() => {
-      mountJSModule(jsModuleCode)
-    }, [jsModuleCode])
-
-    // 初始化js模块默认依赖
-    useEffect(() => {
-      const { sandbox } = connectJSRuntimeVM()
-      sandbox.turboScope.dependencies = {
-        dayjs,
-        '@turbo/store': {
-          getState: () => {
-            console.log('我是get方法')
-          },
-          setState: () => {
-            console.log('我是set方法')
-          },
-        },
-      }
-    }, [])
-
     return (
       <FrameworkContextProvider
         ref={ref}
@@ -58,19 +22,7 @@ export const EditorFramework = forwardRef<EditorRefProps, FrameworkProps>(
           error: '#f95053',
         }}
       >
-        {/* 供予预览直接显示子组件 */}
-        {isPreview ? (
-          <DocumentFrame />
-        ) : (
-          <div className='grid overflow-hidden h-full grid-rows-[auto_1fr]'>
-            <Header />
-            <div id='editorContent' className='h-full grid grid-cols-[auto_1fr_300px]'>
-              <Left />
-              <Canvas />
-              <Right />
-            </div>
-          </div>
-        )}
+        <Editor isPreview={isPreview} />
       </FrameworkContextProvider>
     )
   },
