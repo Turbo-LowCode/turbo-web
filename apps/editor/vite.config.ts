1408ebe8838cfafc4f1dfbf340264da6811f3579
JinSooo <kimjinso@qq.com>
2023-12-11 23:49:19 +0800

    feat(main): ✨ 接入微前端，连接editor

--- apps/editor/vite.config.ts
+++ apps/editor/vite.config.ts
@@ -23,4 +23,7 @@ export default defineConfig({
       '@': path.resolve(__dirname, 'src'),
     },
   },
+  server: {
+    port: 8080,
+  },
 })
--- apps/main/src/pages/micro/index.tsx
+++ apps/main/src/pages/micro/index.tsx
@@ -1,5 +1,11 @@
+import WujieReact from 'wujie-react'
+
 const MicroApp = () => {
-  return <div>MicroApp</div>
+  return (
+    <div className='w-screen h-screen'>
+      <WujieReact width='100%' height='100%' name='editor' url='http://localhost:8080/' sync={true} fetch={fetch} />
+    </div>
+  )
 }
 
 export default MicroApp
