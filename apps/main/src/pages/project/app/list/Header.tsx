247c5853f27a9a069eef39682a109eab4d08ad85
JinSo <kimjinso@qq.com>
2023-12-12 11:21:32 +0800

    feat(main): ✨ 工作台新建应用modal

--- apps/main/src/pages/project/app/list/Header.tsx
+++ apps/main/src/pages/project/app/list/Header.tsx
@@ -1,11 +1,13 @@
 import { Segmented, Space } from 'antd'
-// import { CreateAppModal } from './create-app-modal'
+import { CreateAppModal } from './CreateAppModal'
 
 export const Header = () => {
   return (
     <div className='flex justify-between'>
       <Segmented options={['全部', '与我有关']} />
-      <Space>{/* <CreateAppModal /> */}</Space>
+      <Space>
+        <CreateAppModal />
+      </Space>
     </div>
   )
 }
