9c293864adcac2090ef38e1d0cfae5120485d937
JinSooo <kimjinso@qq.com>
2023-12-12 08:23:51 +0800

    fix(main): 🐛 修复路由懒加载报错

--- apps/main/src/App.tsx
+++ apps/main/src/App.tsx
@@ -1,6 +1,17 @@
+import { LoadingOutlined } from '@ant-design/icons'
+import { Spin } from 'antd'
+import { Suspense } from 'react'
 import { RouterProvider } from 'react-router-dom'
 import { router } from './router'
 
 export const App = () => {
-  return <RouterProvider router={router} />
+  return (
+    <Suspense
+      fallback={
+        <Spin className='absolute right-4 top-4' indicator={<LoadingOutlined style={{ fontSize: 24 }} spin />} />
+      }
+    >
+      <RouterProvider router={router} />
+    </Suspense>
+  )
 }
